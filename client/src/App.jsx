2b0fde3a96eb0040e207078c44d3b53b7e41ffd3
vaisagh01 <vaisuro45@gmail.com>
2024-05-20 16:44:46 +0530

    udpated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
-import { useState } from 'react'
-
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
 function App() {
 
   return (
-    <div>
-      App
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
